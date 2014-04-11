77a626faf52980fe3ca190c45109a24e5a0fcc73
Alistair Jones <alistair.jones@gmail.com>
2014-04-11 14:24:01 +0100

    Use ICNS.

--- packaging/neo4j-desktop/create-dmg.sh
+++ packaging/neo4j-desktop/create-dmg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 title="Neo4j"
-backgroundPictureName="graphs.png"
+backgroundPictureName="graph_background.png"
 applicationName="Neo4j Community"
 finalDMGName="Neo4j.dmg"
 
@@ -24,13 +24,13 @@ echo '
            set current view of container window to icon view
            set toolbar visible of container window to false
            set statusbar visible of container window to false
-           set the bounds of container window to {400, 100, 885, 430}
+           set the bounds of container window to {400, 100, 879, 446}
            set theViewOptions to the icon view options of container window
            set arrangement of theViewOptions to not arranged
            set icon size of theViewOptions to 72
            set background picture of theViewOptions to file ".background:'${backgroundPictureName}'"
-           set position of item "'${applicationName}'" of container window to {100, 100}
-           set position of item "Applications" of container window to {375, 100}
+           set position of item "'${applicationName}'" of container window to {100, 170}
+           set position of item "Applications" of container window to {375, 170}
            update without registering applications
            delay 5
            eject
