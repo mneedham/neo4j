6622b94af769b051ec5ca692850c7a8cea3b5c28
Mark Needham <mark.needham@neotechnology.com>
2016-06-02 16:51:49 +0100

    wip

--- packaging/standalone/src/tests/shell-scripts/test-restore.sh
+++ packaging/standalone/src/tests/shell-scripts/test-restore.sh
@@ -5,13 +5,14 @@ test_description="Test neo4j-admin restore"
 . ./lib/sharness.sh
 fake_install
 
-test_expect_success "should invoke restore main class, pass in database name, config directory and from directory" "
-  neo4j-home/bin/neo4j-admin restore --from=backup.db --database=foo.db  &&
+test_expect_success "should invoke restore main class and pass through all arguments" "
+  neo4j-home/bin/neo4j-admin restore --from=backup.db --database=foo.db --force  &&
   test_expect_java_arg 'org.neo4j.restore.RestoreDatabaseCli' &&
   test_expect_java_arg '--home-dir=$(neo4j_home)' &&
   test_expect_java_arg '--database=foo.db' &&
   test_expect_java_arg '--from=backup.db' &&
   test_expect_java_arg '--config=$(neo4j_home)/conf'
+  test_expect_java_arg '--force'
 "
 
 test_done
